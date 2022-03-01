dc7469bef9722968065aa2e7c4496e09955ef3f7
ganpanor <ganpanor@gmail.com>
2022-03-02 01:21:38 +0900

    #5

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,37 @@
-export const trending = (req, res) => res.send("alalal");
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "Video #1",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 min ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Video #2",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 min ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Video #3",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 min ago",
+      views: 59,
+      id: 1,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
+};
 export const see = (req, res) => {
-  return res.send(`i watching #${req.params.id}`);
+  return res.render("watch", { pageTitle: "Watch" });
 };
 export const edit = (req, res) => {
-  console.log(req.params);
-  return res.send("edit");
+  return res.render("edit", { pageTitle: "Edit" });
 };
 export const search = (req, res) => res.send("search");
 export const upload = (req, res) => res.send("upload");
